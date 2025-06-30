ef6623f355c7031cc99c15a97591875ab57aed31
AltyIbrhm <alty.ibrhm@gmail.com>
2025-06-30 19:55:50 -0400

    Fix: ignore uncaught exceptions in Qualitest home page test to prevent Cypress failures on site JS errors

--- cypress/e2e/qualitest_home.cy.js
+++ cypress/e2e/qualitest_home.cy.js
@@ -1,5 +1,9 @@
 describe('Qualitest Careers Home Page', () => {
   it('should display the Qualitest logo', () => {
+    cy.on('uncaught:exception', (err, runnable) => {
+      // returning false here prevents Cypress from failing the test
+      return false;
+    });
     cy.visit('https://careers.qualitestgroup.com/');
     cy.get('header').should('be.visible');
     cy.contains('Qualitest').should('be.visible');
