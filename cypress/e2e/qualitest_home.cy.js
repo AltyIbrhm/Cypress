4038eb18b340a8dc6f22c68a6893c3d9a8a17b44
AltyIbrhm <alty.ibrhm@gmail.com>
2025-06-30 19:58:27 -0400

    Fix: update Qualitest home page test to use reliable selector and longer timeout

--- cypress/e2e/qualitest_home.cy.js
+++ cypress/e2e/qualitest_home.cy.js
@@ -1,11 +1,10 @@
 describe('Qualitest Careers Home Page', () => {
-  it('should display the Qualitest logo', () => {
+  it('should display the Qualitest brand text', () => {
     cy.on('uncaught:exception', (err, runnable) => {
       // returning false here prevents Cypress from failing the test
       return false;
     });
     cy.visit('https://careers.qualitestgroup.com/');
-    cy.get('header').should('be.visible');
-    cy.contains('Qualitest').should('be.visible');
+    cy.contains('Qualitest', { timeout: 10000 }).should('be.visible');
   });
 }); 
