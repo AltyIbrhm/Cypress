1bdd9e31d66fc6cddf34e12409f32dea13109b6e
AltyIbrhm <alty.ibrhm@gmail.com>
2025-07-01 13:22:47 -0400

    Trrigger CI/CD pipeline

--- cypress/e2e/api_test.cy.js
+++ cypress/e2e/api_test.cy.js
@@ -1,4 +1,4 @@
-describe('API Testing Suite1', () => {
+describe('API Testing Suite', () => {
   const baseUrl = 'https://jsonplaceholder.typicode.com'; // Real test API
   const apiKey = Cypress.env('API_KEY') || 'your-api-key-here';
   
